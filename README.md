b4291472e280ca88fe50c4de5a2aa428f86da46d
RajasathyavanS <42690449+RajasathyavanS@users.noreply.github.com>
2018-08-26 09:37:05 +0530

    Update and rename README.md to config.js

--- README.md
+++
@@ -1,4 +0,0 @@
-# cr-ud-operation
-
-HAI,
-    My name is S.Rajasathyavan. I have task to finish CRUD operation by using mongodb and  node.js.
---
+++ config.js
@@ -0,0 +1,12 @@
+# cr-ud-operation
+
+var config = {
+    database: {
+        url: 'mongodb://localhost:27017/test'
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+ 
